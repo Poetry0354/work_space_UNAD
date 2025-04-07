cb4d748048b8e4da5b7967b6587840a8bf2312f8
Poetry0354 <strujilloespinosa@gmail.com>
2025-04-07 12:39:55 -0500

    etapa3

---
+++ ejercicios_grupo4_tarea3/ejercicio1.py
@@ -0,0 +1,10 @@
+"""
+
+Realizar el siguiente ejercicio con la instrucción if..else
+En el Black Friday, la tienda de artículos deportivos “Sports” va a
+realizar un descuento del 25% en toda su línea de productos si el valor
+es mayor a 200000, le han solicitado a usted que construya un
+programa que solicite el valor del producto a comprar, muestre el valor
+del descuento si aplica y el valor final a pagar.
+
+"""
