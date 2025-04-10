645ab787472b810fa3bbd2ce0496658adb713c91
Poetry0354 <strujilloespinosa@gmail.com>
2025-04-10 18:18:42 -0500

    Ejercicio Final número 4

---
+++ ejercicios_grupo4_tarea3/ultimoEjercicio4.py
@@ -0,0 +1,24 @@
+"""
+
+Realizar el siguiente ejercicio con la instrucción if..else
+mediante condicionales anidados (es decir dentro de la
+instrucción else colocar una nueva condición if..else)
+
+
+Los alumnos del profesor Juan requieren un programa que les indique si
+han aprobado el curso. Para esto, usted debe solicitar la nota definitiva
+y si dicha nota es menor a 3, muestre un mensaje indicando que no ha
+aprobado el curso. Si es igual 3 y menor a 4.5 debe mostrar un mensaje
+que informe que el alumno ha aprobado el curso, si la nota es mayor a
+4.5 muestre el mensaje que ha probado el curso con un desempeño
+superior.
+
+
+"""
+
+print(
+    "Bienvenidos estudiantes de la UNAD, a continuación se les indicará si han aprobado el curso"
+)
+print("Por favor ingrese su nota definitiva: ")
+
+nota = float(input("Nota: "))
