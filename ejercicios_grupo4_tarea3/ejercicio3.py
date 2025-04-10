7b96e1db8164031d23b3b15553552998120794f9
Poetry0354 <strujilloespinosa@gmail.com>
2025-04-10 12:42:59 -0500

    Cambios de Ejercicios.
    En este caso se hace commit para continuar con la secuencia de ejercicios con su despectiva documentación para dar a conocer el flujo de trabajo

--- ejercicios_grupo4_tarea3/ejercicio3.py
+++ ejercicios_grupo4_tarea3/ejercicio3.py
@@ -16,27 +16,33 @@ clasificación.
 
 """
 
+# Imprime un mensaje de bienvenida al usuario
 print("Bienvenido a la convocatoria, estamos muy feciles de tenerte, ;)")
 print()
 
+# Explica el propósito del programa y solicita al usuario que ingrese datos
 print(
     "Vamos a empezar la convocatoria, por favor digite en numeros la cantidad de goles que a recibido en su contra: "
 )
 
+# Captura la cantidad de goles recibidos en los últimos 5 partidos como un número entero
 goles_recibidos = int(
     input("Ingrese la cantidad de goles recibidos en los últimos 5 partidos: ")
 )
 
+# Evalúa la cantidad de goles recibidos para determinar el nivel del jugador
 if goles_recibidos > 12:
-    nivel = "BAJO"
+    nivel = "BAJO"  # Nivel bajo si los goles recibidos son mayores a 12
 elif 5 <= goles_recibidos <= 12:
-    nivel = "MEDIO"
+    nivel = "MEDIO"  # Nivel medio si los goles recibidos están entre 5 y 12
 elif 0 <= goles_recibidos <= 4:
-    nivel = "ALTO"
+    nivel = "ALTO"  # Nivel alto si los goles recibidos están entre 0 y 4
 else:
+    # Maneja el caso de entrada inválida (número negativo)
     nivel = "INVALIDO"
     print("El número de goles ingresado no es válido. Debe ser un número positivo.")
-    exit()
+    exit()  # Termina el programa si la entrada es inválida
 
+# Muestra el nivel del jugador basado en la evaluación anterior
 print("El nivel de su juego es el siguiente: ", nivel)
 print("Gracias por participar en la convocatoria")
