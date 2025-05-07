30b02f00e368c4c4f06aa7807c965396900e241c
Poetry0354 <strujilloespinosa@gmail.com>
2025-05-07 11:25:28 -0500

    Agregar descripción del ejercicio para calcular la propina sugerida en el restaurante Doña Pancha.

---
+++ ejercicio_grupo3_tarea4/ejercicio2.py
@@ -0,0 +1,28 @@
+"""
+*Resolver el siguiente ejercicio con el ciclo while:*
+*2. El restaurante “doña Pancha” desea construir un programa
+*para calcular el valor de la propina sugerida para n clientes.
+*Para cada cliente, el programa debe solicitar el valor de la
+*cuenta, calcular el 15% de ese valor y mostrar así el valor de
+*la propina sugerida. Al finalizar el registro de cada cliente, se
+*debe preguntar si desea registrar un nuevo cliente. El
+*programa finaliza cuando el usuario responda que no desea
+*registrar un nuevo cliente.
+
+! Requerimientos adicionales (casos de uso)
+    * 1. Solicitar un referido al momento de mostrar la factura, es voluntario:
+    * Si se selecciona que si, se solicita nombre y telefono.
+    * 2. Total de ventas del dia.
+    * 3. Si paga con propina o no.
+    * 4. Factura electronica o no.
+    * 5. Sugerencias (almacenar)
+"""
+
+# Mensaje de agradecimiento por el uso del servicio.
+print("\n" + "="*50)
+print("  Bienvenido a Restaurante Doña Pancha".center(50))
+print("  Donde servimos con calor de hogar".center(50))
+print("="*50)
+print("\nRegistraremos los datos necesarios para el recibo del cliente.".center(50))
+print("\n" + "="*50)
+
