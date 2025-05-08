edb4401a465b0d6246abe8773eb3fa9c416e47fa
Poetry0354 <strujilloespinosa@gmail.com>
2025-05-08 06:03:55 -0500

    Creación del ejercicio con su descripción

---
+++ ejercicio_grupo3_tarea4/ejercicio3.py
@@ -0,0 +1,10 @@
+"""
+Ciclo White.
+3. La empresa Netflix desea saber en una encuesta a 15 personas
+para saber cuántas horas promedio invierten en mirar sus
+contenidos los domingos. Para esto, usted debe construir un
+programa que solicite la cantidad de horas que invierte cada
+persona encuestada el domingo, almacenando ese dato en una
+lista y que muestre el promedio de horas de todo el grupo.
+
+"""
